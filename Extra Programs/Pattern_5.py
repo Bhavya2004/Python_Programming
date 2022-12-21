5dbcf84156b4d7bc96ccfda4c27b0826e59f4e8c
Bhavya Barai <bhavyabarai85@gmail.com>
2022-12-21 17:52:11 +0530

    21-12-2022

--- Extra Programs/Pattern_5.py
+++ Extra Programs/Pattern_5.py
@@ -5,13 +5,18 @@
 # 5 4 4 4 5
 # 5 5 5 5 5
 
-# [google's logic]:
-# n=int(input("Enter the value of n: "))
-# for x in range(1-n,n):
-#     for y in range(1-n,n):
-#         if abs(x)>abs(y):
-#             print("{:2d}".format(abs(x)+1),end="")
-#         else:
-#             print("{:2d}".format(abs(y)+1),end="")
-#     print()
+
+n=int(input("Enter the value of n: "))
+
+for x in range(1,n+1):
+    for y in range(1,n+1):
+        if((y == 1 or y == n) or (x == 1 or x == n)):
+            print(n,end="")
+        elif(x==2 or x==n-1 or y==2 or y ==n-1):
+            print(n-1,end="")
+        else:
+            print(n-2,end="")
+        
+    print()
+    
         
